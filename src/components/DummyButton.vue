4c7dc624931df47a9cfca7b12c59c3e633227585
Wang, Jian-Zhong <jzwang.dev@gmail.com>
2022-05-05 20:34:58 +0800

    init commit

---
+++ src/components/DummyButton.vue
@@ -0,0 +1,16 @@
+<template>
+  <button type="button" @click="count++">clicked {{ count }} time(s)</button>
+</template>
+
+<script>
+export default {
+  name: "DummyButton",
+
+  data() {
+    return {
+      count: 0,
+    };
+  },
+};
+</script>
+
